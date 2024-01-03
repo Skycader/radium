13197d9dfc8e0d3dd15213f6ff80354424b19726
skycader <skycader@yandex.ru>
2024-01-03 23:04:11 +0300

    lesson_1_complete

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthComponent } from './components/auth/auth.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+
+const routes: Routes = [
+  {
+    path: 'auth',
+    component: AuthComponent,
+  },
+];
+
+@NgModule({
+  declarations: [AuthComponent],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule.forChild(routes)],
+})
+export class AuthModule { }
