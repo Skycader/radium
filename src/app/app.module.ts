1f04ba347ecbeb4b7d26efd5f0f382ee878f40f7
skycader <skycader@yandex.ru>
2024-01-04 22:59:01 +0300

    lesson-4-reafy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
 import { NotFoundModule } from './not-found/not-found.module';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
@@ -18,6 +20,10 @@ import { StoreModule } from '@ngrx/store';
     AuthModule,
     NotFoundModule,
     StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
   ],
   providers: [provideClientHydration()],
   bootstrap: [AppComponent],
